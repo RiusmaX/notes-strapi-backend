5b743af88221289ba10be4a63cfaab8c1987266f
kerhac <marius@myidea.fr>
2021-02-03 12:08:07 +0100

    Add Notes

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -4,17 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: env('DATABASE_URI')
-        // host: env('DATABASE_HOST', 'strapi-cluster.micdw.mongodb.net'),
-        // srv: env.bool('DATABASE_SRV', true),
-        // port: env.int('DATABASE_PORT', 27017),
-        // database: env('DATABASE_NAME', 'backend'),
-        // username: env('DATABASE_USERNAME', 'strapi'),
-        // password: env('DATABASE_PASSWORD', 'DZ4959pqifGYsxE3'),
+        host: env('DATABASE_HOST', 'strapi-cluster.micdw.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'backend'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'DZ4959pqifGYsxE3'),
       },
       options: {
-        // authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: true // env.bool('DATABASE_SSL', true),
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
